04264e620a236f23d6ca891f96bba1a0246d9403
harshbhati141 <harshbhati141@gmail.com>
2024-08-26 20:36:57 +0530

    Update Node1_RNG.h

--- Data_Transmission_from_Node1/Node1_RNG.h
+++ Data_Transmission_from_Node1/Node1_RNG.h
@@ -1,13 +1,5 @@
-/*
- * 
- */
-
-#ifndef INC_DISC_F407_H_
-#define INC_DISC_F407_H_
-
 #include <stdint.h>
 
 void rngInit(void);
 uint32_t StmRand( uint32_t Max, uint32_t Min );
 
-#endif /* INC_DISC_F407_H_ */
